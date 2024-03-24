b98c47091765847cbb27a3bb1b16dc58353d7693
psyen1 <psyen1@exmail.nottingham.ac.uk>
2024-03-24 00:00:12 +0000

    Initial Commit

---
+++ Visuals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visuals
+//
+//  Created by Edwin Nwosu on 24/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visuals/VisualsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualsApp.swift
+//  Visuals
+//
+//  Created by Edwin Nwosu on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct VisualsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
